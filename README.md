25e2f182de9b59f17448c50663aee7ff1f63b6dd
Gregg Lind <gregg.lind@gmail.com>
2016-09-22 21:00:24 -0500

    v0.5.0 which is feature complete Fix #1 #2 #4 #5 #6 #7 #8

--- README.md
+++ README.md
@@ -20,12 +20,11 @@ $ npm install -g observatory-cli
 1.  **Scan a site** for `https` best practices
 
     ```
+    # json!
     $ observatory some.site.name
 
-
-    # include 'zero' scores, display as csv
-    $ observatory some.site.name --zero --csv
-
+    # include 'zero' scores, display as a tabular report
+    $ observatory some.site.name --zero --format=report
 
     # attempt to force a re-scan
     $ observatory some.site.name --rescan
@@ -37,50 +36,107 @@ $ npm install -g observatory-cli
     Script will FAIL unless the grade is AT LEAST `B+`
 
     ```
-    $ observatory some.site.name --expect B+
+    $ observatory some.site.name --min-grade B+
     ```
+
+    ...and the score is at least 50.
+
+    ```
+    $ observatory some.site.name --min-grade B+ --min-score 50
+    ```
+
 
 3.  **Show the URL** for report
 
     ```
-    $ observatory some.site.name --web
+    $ observatory some.site.name --format=url
     ```
+
+4.  **nagios** monitoring plugin
+
+    `--min-score`, `--min-grade`, `--zero`, `--skip` aftect the test.
+
+    ```
+    $ observatory  --nagios 2 --min-score 85 -z --skip cookies
+    CRITICAL ["content-security-policy",...,"x-xss-protection"]
+    ```
+
+    If neither `--min-score` nor `--min-grade` is specified, any
+    non-zero scores fail the test.
 
 ## Help
 
 ```
 $ observatory --help
 
-  Usage: observatory [options] <site>
+  Usage: index [options] <site>
+
+  cli for interacting with Mozilla HTTP Observatory
+
+  https://observatory.mozilla.org/
 
   Options:
 
-    -h, --help        output usage information
-    -V, --version     output the version number
-    --rescan          initiate a rescan instead of showing recent scan results
-    -z --zero         show test results that don't affect the final score
-    --csv             format report as csv
-    --web             print the url for the report and exit
-    --expect <grade>  testing: grade to expect / demand, or fail
-    --attempts <n>    number of attempts to try before failing
-    --tls             do tls checks instead [TODO]
+    -h, --help               output usage information
+    -V, --version            output the version number
+    --format [format]        format for output.  choice:  (json|report|csv|url).  `json` is default
+    --min-grade <grade>      testing: this grade or better, or exit(1)
+    --min-score <score>      testing: this score or better, or exit(1)
+    --nagios [failcode]      nagios mode, exits with [failcode] on failure
+    --rescan                 initiate a rescan instead of showing recent scan results
+    -z, --zero               show test results that don't affect the final score
+    --attempts <n>           number of attempts to try before failing
+    --api-version [version]  api version:  defaults to 1
+    --skip <rule>            skip rules by name.  works with min-score only
+    --tls                    do tls checks instead
+    -q, --quiet              turns off all logging
+
+
+Formats help:
+    - json    json of the report
+    - report  plain-text tabular format
+    - csv     alias for report
+    - url     url for online version
+
+
+Nagios Mode (--nagios)
+  - if `--min-score` and/or `--min-grade`, use those.
+  - else *any* failing rules fail the check.
+  - exits with integer `failcode`.
 ```
 
 
 ## Example Results
 
+Added on:
+
+* `-z` to show '0' things
+* `--skip` to skip a rule (affecting SCORE, but not GRADE)
+
 ```
-$ observatory some.site --csv
+$ observatory some.site --format=report -z --skip redirection
+
+observatory [INFO] modfiying score, because of --skip.  was: 60, now: 65
+
+HTTP Observatory Report: some.site
 
 Score Description
 
-   25 Content Security Policy (CSP) header not implemented
-   10 HTTP Strict Transport Security (HSTS) header set to less than six months (15768000)
-    5 Contribute.json file missing from root of website
+  -20 content-security-policy        Content Security Policy (CSP) implemented, but allows 'unsafe-inline' inside script-src
+  -10 x-xss-protection               X-XSS-Protection header not implemented
+   -5 x-content-type-options         X-Content-Type-Options header not implemented
+    0 contribute                     Contribute.json implemented with the required contact information
+    0 cookies                        No cookies detected
+    0 cross-origin-resource-sharing  Content is not visible via cross-origin resource sharing (CORS) files or headers
+    0 public-key-pinning             HTTP Public Key Pinning (HPKP) header not implemented
+    0 strict-transport-security      HTTP Strict Transport Security (HSTS) header set to a minimum of six months (15768000)
+    0 subresource-integrity          Subresource Integrity (SRI) not implemented, but all scripts are loaded from a similar origin
+    0 x-frame-options                X-Frame-Options (XFO) header set to SAMEORIGIN or DENY
 
-## full report at:
+Score: 65 (modified due to --skip)
+Grade: C+
 
-  https://observatory.mozilla.org/analyze.html?host=some.site
+Full Report Url: https://observatory.mozilla.org/analyze.html?host=some.site
 
 ```
 
--- index.js
+++ index.js
@@ -1,72 +1,168 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
+
+"use strict";
 
 // https://github.com/mozilla/http-observatory-cli/blob/master/httpobscli/cli.py
 // we are doing a lot of things in a sillier way
 
-var chalk = require('chalk');
-var program = require('commander');
+var chalk = require("chalk");
+var program = require("commander");
 var request = require("request");
-var rp = require('request-promise');
-var delay = require('timeout-as-promise');
-var merge = require('merge');
+var rp = require("request-promise");
+var delay = require("timeout-as-promise");
+var merge = require("merge");
 var sprintf=require("sprintf-js").sprintf;
+var util = require("util");
 
+var SITE_URL = "https://observatory.mozilla.org/";
 var API_URL = process.env.HTTPOBS_API_URL || "https://http-observatory.security.mozilla.org/api/v1/";
 
-var attempts = 0;
-function promiseScan (site, options) {
-  attempts += 1;
-  allowed = options.attempts || 10;
-  if (attempts > allowed) {
-    throw new Error(sprintf("too many attempts %s", attempts));
-  }
-  var url = API_URL + "analyze?host=" + site;
+/* utilities and ephemera */
+var clim = require("clim");
 
-  var qargs = {site: site};
-  ['rescan', 'zero'].forEach(function (k) {
-    if (options[k]) {
-      qargs[k] = 'true'
-    }
-  })
-  return rp.post({
-      url: url,
-      json: true,
-      simple: true,
-      formData: qargs
-  }).then(
-    function (scan) {
-      if (options.rescan && (scan.error == 'rescan-attempt-too-soon')) {
-        //console.log(scan);
-        console.warn('Rescan attempt is sooner than the allowed cooldown period. Returning cached results instead.')
-        options.rescan = false;
-        return promiseScan(site, options)
-      } else {
-        if (scan.error) {
-          throw new Error(scan.error)
-        }
-      }
-      //console.log(scan) // Print the json response
-      if (scan.state === 'FINISHED') {
-        return scan
-      } else {
-        console.warn(sprintf('retrying in 1 second (attempt %s/%s)', attempts, allowed));
-        return delay(1000).then(function () {return promiseScan(site, options)})
-      }
-    }
-  )
+var QUIET = false;
+var hasSite = false;
+
+function padright(s,l) {
+  if (s.length >= l) return s;
+  return s + Array(l).fill(" ").join("").slice(s.length)
 }
 
+clim.logWrite = function(level, prefixes, msg) {
+  if (QUIET) return
+
+  var pfx = "";
+  if (prefixes.length > 0) pfx = prefixes.join(" ");
+
+  level = (f[level.toLowerCase()] || function (p) {return p})(level);
+  var line = util.format("%s [%s] %s", pfx, level, msg)
+  switch (level) {
+    case "ERROR":
+      process.stderr.write(line + "\n");
+      break
+    default:
+      process.stdout.write(line + "\n");
+  }
+};
+
+var logger = clim(chalk.grey("observatory"));
+
+var f = {
+  error: chalk.red,
+  link:  chalk.underline.blue,
+  info:  chalk.blue,
+  log:   chalk.blue,
+  warn:  chalk.red,
+  bold:  chalk.bold,
+  code:  chalk.grey,
+  header: chalk.bold.blue
+}
+
+function collect(val, O) {
+  O[val] = true;
+  return O;
+}
+
+function helpAnywhere(rawArgs) {
+  return rawArgs.filter(function (x) { return /-h|--help/.exec(x)}).length
+}
+
+var FORMATS = {
+  "json": {
+    "description": "json of the report",
+    "action": function (scores) { console.log(JSON.stringify(scores,null,2))}
+  },
+  "report": {
+    "description": "plain-text tabular format",
+    "action": formatAnswersCsv
+  },
+  "csv": {
+    "description": util.format("alias for %s", f.code("report")),
+    "action": formatAnswersCsv
+  },
+  "url":{
+    "description": "url for online version",
+    // this one we handle early, so it has a different api
+    "action": function () {}
+  }
+};
+
+function validateFormatChoice (given) {
+  given = given.toLowerCase();
+  if (given in FORMATS) return given
+  else {
+    logger.error("not a valid format choice: %s.  Allowed: ",
+      f.error(given),
+      f.code(Object.keys(FORMATS).join("|")))
+    process.exit(1)
+  }
+}
+
+function longestInList (L) {
+  return Math.max.apply(null, L.map(function(x) {return x.length}))
+}
+
+/* functions for retrieving reports */
+class Scanner {
+  constructor (site, options) {
+    this.attempts = 0;
+    this.allowed = options.attempts || 10;
+    this.site = site;
+  }
+  promiseScan (site, options) {
+    var that = this;
+    this.attempts += 1;
+    site = site || this.site;
+    options = options || this.options;
+
+    if (this.attempts >= this.allowed) {
+      throw new Error(sprintf("too many attempts %s", this.attempts));
+    }
+    var url = API_URL + "analyze?host=" + site;
+
+    var qargs = {site: site};
+    ["rescan", "zero"].forEach(function (k) {
+      if (options[k]) {
+        qargs[k] = "true"
+      }
+    })
+    return rp.post({
+        url: url,
+        json: true,
+        simple: true,
+        formData: qargs
+    }).then(
+      function (scan) {
+        if (options.rescan && (scan.error == "rescan-attempt-too-soon")) {
+          logger.warn("Rescan attempt is sooner than the allowed cooldown period. Returning cached results instead.")
+          options.rescan = false;
+          return that.promiseScan(site, options)
+        } else {
+          if (scan.error) {
+            throw new Error(scan.error)
+          }
+        }
+        if (scan.state === "FINISHED") {
+          return scan
+        } else {
+          logger.warn(sprintf("retrying in 1 second (attempt %s/%s)", that.attempts, that.allowed));
+          return delay(1000).then(function () {return that.promiseScan(site, options)})
+        }
+      }
+    )
+  }
+}
 
 /** related to grades */
-var grades = ['F'];
-'DCBA'.split('').forEach(function (g) {
-  ['-','','+'].forEach(function(mark) {
+var grades = ["F"];
+"DCBA".split("").forEach(function (g) {
+  ["-"," ","+"].forEach(function(mark) {
     grades.push(g+mark);
   })
 })
 
 function gradeCompare (seen, wanted) {
-  return grades.indexOf(wanted) >= grades.indexOf(seen)
+  return grades.indexOf(wanted) <= grades.indexOf(seen)
 }
 
 function validateGrade (val) {
@@ -79,90 +175,168 @@ function validateGrade (val) {
 }
 
 function openSite (url) {
-  console.log(url);
-  // var opener = require("opener");
-  // opener("http://google.com");
+  logger.log(f.link(url));
+
 }
 
 function promiseReport (scan, options) {
   var url = API_URL + "getScanResults?scan=" + scan.scan_id;
-  //console.log(url);
-
-  // side cases
-  if (options.expect) {
-    if (!gradeCompare(scan.grade, options.expect)) {
-      throw new Error(sprintf("bad grade.  wanted %s, got %s",  options.expect, scan.grade))
-    }
-    return
-  }
 
   return rp({
       url: url,
       json: true,
       simple: true
     }).then(function (reportData) {
-      printReport(reportData, url, options)
+      formatAnswer(reportData, url, scan, options)
     }
   )
 }
 
-function printReportsCsv (scoresList, url, options) {
-  // print('[{modifier:>4}] {reason}'.format(modifier=score[0], reason=score[1].replace('"','\\"')))
-
-  console.log("\nScore Description\n");
-
-  scoresList.map(function (score) {
-    console.log(sprintf("%5f %s", score.score_modifier, score.score_description))
-  })
-
-  var fullReportUrl = "https://observatory.mozilla.org/analyze.html?host=" + options.site;
-
-  console.log(sprintf("\n## full report at: \n\n  %s\n", fullReportUrl))
-}
-
-function printReport(reportData, url, options) {
-  function invertScore(k) {
-    var score = k.score_modifier;
-    if (score < 0) {
-      k.score_modifier = -score;
-    }
-    return k
-  }
+function formatAnswersCsv (scores, url, scan, options) {
+  // print("[{modifier:>4}] {reason}".format(modifier=score[0], reason=score[1].replace(""","\\"")))
 
   function sortByScore(a,b) {
     return a.score_modifier - b.score_modifier
   }
 
-  var scores = {};
+  var longest = longestInList(Object.keys(scores));
+
   var scoresList = [];
-
-  // option --zero shows all.
-  if (!options.zero) {
-    for (var k in reportData) {
-      var v = reportData[k]
-      if (v.score_modifier != 0) {
-        scores[k] = v;
-      }
-    }
-  } else {
-    scores = reportData;
-  }
-
-  // turn zero-filtered into a list
   for (var k in scores) {
     scoresList.push(scores[k])
   }
 
   // sort and invert to positive
-  scoresList.sort(sortByScore).map(invertScore)
+  scoresList.sort(sortByScore)
 
-  if (options.csv) {
-    printReportsCsv(scoresList, url, options);
+  console.log("\n%s: %s", f.header("HTTP Observatory Report"), options.site)
+
+  console.log("\n%s\n", f.header("Score Description"));
+
+
+  scoresList.map(function (score) {
+    console.log(sprintf("%5f %s %s", score.score_modifier, f.error(padright(score.name, longest+1)), score.score_description))
+  })
+
+  var fullReportUrl = "https://observatory.mozilla.org/analyze.html?host=" + options.site;
+
+  if (Object.keys(options.skip).length) {
+    console.log("\nScore: %s (modified due to --skip)", scan.score);
+  } else {
+    console.log("\nScore: %s", scan.score);
   }
-  else {
-    console.log(scores)
+  console.log("Grade: %s", scan.grade);
+
+  console.log(sprintf("\n%s: %s\n", f.header("Full Report Url"), f.link(fullReportUrl)));
+}
+
+function handleNagiosMode(options, scan, scores) {
+  // https://github.com/mozilla/http-observatory-cli/issues/6
+
+  var nagiosExitCodes = {
+    1: "WARNING",
+    2: "CRITICAL",
+    3: "UNKNOWN"
+  };
+
+  if (!(options.nagios in nagiosExitCodes)) {
+    throw new Error("nagios exit code must be in %s", Object.keys(nagiosExitCodes).join("|"))
+  }
+  var codeName = nagiosExitCodes[options.nagios];
+
+  function OK () {
+    console.log("OK")
+    process.exit(0)
   }
 
+  function FAIL () {
+    console.log("%s %j", codeName, Object.keys(scores))
+    process.exit(codeName)
+  }
+
+  if (options.minGrade || (options.minScore !== undefined)) {
+    try {
+      handleExpectedScore(options, scan)
+      OK();
+    } catch (err) {
+      FAIL();
+    }
+    return
+  } else {
+    if (Object.keys(scores).length) {
+      FAIL()
+    } else {
+      OK()
+    }
+  }
+}
+
+function handleExpectedScore (options, scan) {
+  // side cases
+  var mingrade = options.minGrade,
+      minscore = options.minScore;
+
+  if (mingrade) {
+    if (!gradeCompare(scan.grade, mingrade)) {
+      throw new Error(sprintf("bad grade.  wanted %s, got %s",  mingrade, scan.grade))
+    }
+  }
+  if (minscore !== undefined) {
+    if (scan.score < minscore) {
+      throw new Error(sprintf("bad score.  wanted %s, got %s",  minscore, scan.score))
+    }
+  }
+  return
+}
+
+function formatAnswer(reportData, url, scan, options) {
+  /** Reporting: */
+  var scores = {};
+  /* filter out --zeros, and --skip or not */
+  for (var k in reportData) {
+    var v = reportData[k]
+    // --zero
+    if (v.score_modifier != 0 || options.zero) {
+      // --skip
+      if (k in options.skip) { continue }
+
+      // actually copy
+      scores[k] = v;
+    }
+  }
+
+  // rescore
+  var newScore = 100;
+  for (var k in scores) {
+    newScore += scores[k].score_modifier;
+  }
+
+  if (Object.keys(options.skip).length) {
+    logger.info("modfiying score, because of --skip.  was: %s, now: %s", f.info(scan.score), f.info(newScore))
+    scan.score = newScore;
+  }
+
+  if (options.nagios != undefined) {
+    // nagios has
+    try {
+      handleNagiosMode(options, scan, scores)
+    } catch (err) {
+      logger.error(err);
+      process.exit(1)
+    }
+  }
+
+  if (options.minGrade || (options.minScore !== undefined)) {
+    try {
+      handleExpectedScore(options, scan)
+    } catch (err) {
+      logger.error(err);
+      process.exit(1)
+    }
+    return
+  }
+
+  FORMATS[options.format || "json"].action(scores, url, scan, options)
 }
 
 var passedOn = [];
@@ -180,38 +354,108 @@ function preprocess (args) {
   return out;
 }
 
-var program = require('commander');
+class Observatory {
+  handleNoSite (options) {
+    if (options.tls) {
+      logger.info("--tls is not yet implemented.")
+      logger.info("website:   ",f.link("https://observatory.mozilla.org/"))
+      logger.info("go client: ", f.link("https://github.com/mozilla/tls-observatory"))
+      logger.info("comment:    %s", f.link("https://github.com/mozilla/observatory-cli/issues/5"))
+      return
+    };
+
+    // require site for everything else
+    logger.error(f.error("no <site> given"));
+    return program.help();
+  }
+}
+
+let O = new Observatory();
+
+var program = require("commander");
 
 program
-  .version('0.2.1')
+  .version(require("./package.json").version)
+  .description(util.format("cli for interacting with Mozilla HTTP Observatory \n\n  %s", f.link(SITE_URL)))
 
 program
-  //.command('check <site>')
-  .arguments('<site>')
+  //.command("check <site>")
+  .arguments("<site>")
+  .option("--format [format]", util.format("format for output.  choice:  (%s).  `json` is default",
+    Object.keys(FORMATS).join("|")),
+    validateFormatChoice)
+  .option("--min-grade <grade>", "testing: this grade or better, or exit(1)", validateGrade)
+  .option("--min-score <score>", "testing: this score or better, or exit(1)", Number)
+  .option("--nagios [failcode]", "nagios mode, exits with [failcode] on failure", Number)
   .option("--rescan", "initiate a rescan instead of showing recent scan results")
-  .option("-z --zero", "show test results that don't affect the final score")
-  .option("--csv", "format report as csv")
-  .option("--web", "print the url for the report and exit")
-  .option("--expect <grade>", "testing: grade to expect / demand, or fail", validateGrade)
+  .option("-z, --zero", "show test results that don't affect the final score")
   .option("--attempts <n>", "number of attempts to try before failing", Number)
-  .option("--tls", "do tls checks instead [TODO]")
+  .option("--api-version [version]", "api version:  defaults to 1", Number)
+  .option("--skip <rule>", "skip rules by name.  works with min-score only", collect, {})
+  .option("--tls", "do tls checks instead")
+  .option("-q, --quiet", "turns off all logging", function () {QUIET=true})
 
-  .action(function (site, options) {
+  .action(function mainAction(site, options) {
+    // everything here has a site
+    hasSite = true;
+
+    if (helpAnywhere(options.rawArgs)) program.help();
+
     options.site = site; // stuff it in.
 
     if (options.tls) {
-      throw new Error ("tls, attempts not yet implemented")
-    };
-
-    if (options.web) {
-      return openSite("https://observatory.mozilla.org/analyze.html?host=" + site)
+      return O.handleNoSite(options);
+    }
+    if (options.format == "url") {
+      return openSite(util.format("https://observatory.mozilla.org/analyze.html?host=%s", site))
     }
 
-    promiseScan(site, options).then(
+    var api = options.apiVersion
+    if (api && api !== 1) {
+      logger.error(sprintf("V1 is only API supported, not %s", api));
+      process.exit(1)
+    }
+
+    var S = new Scanner(site, options);
+    S.promiseScan(site, options).then(
     function (reportId) {promiseReport(reportId, options)}).
     catch(function (err){
-      console.error(err)
+      logger.error(err);
+      process.exit(1)
     })
+
   })
 
+function newHelp () {
+  var longest = longestInList(Object.keys(FORMATS));
+
+  var out =
+    "\nFormats help:\n" +
+
+    Object.keys(FORMATS).map(
+      function (k) {
+        return util.format("    - %s  %s\n",
+          f.info(padright(k,longest)),
+          FORMATS[k].description
+        )
+      }).join("") +
+
+    "\n\n" +
+
+    f.header("Nagios Mode" ) +" " + f.code("(--nagios)") +
+    "\n" +
+    "  - if `--min-score` and/or `--min-grade`, use those.\n" +
+    "  - else *any* failing rules fail the check.\n" +
+    "  - exits with integer `failcode`.\n"
+
+  return out;
+}
+
+program.on("--help", function(){
+  console.log(newHelp());
+});
+
 program.parse(preprocess(process.argv));
+if (! hasSite) {
+  O.handleNoSite(program);
+}
